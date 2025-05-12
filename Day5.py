dcb9db6cd804e1124b353786a6830436d8efaf36
dewangupadhyay4 <dewangupadhyay23@gmail.com>
2025-05-12 13:11:54 +0530

    This is Day6 of learning python an this is about tuple and sets

---
+++ Day5.py
@@ -0,0 +1,20 @@
+fruits=["Apple", "Watermelon","Banana", "Grapes", "Mango"]
+for i in fruits:
+    print(i)
+new_list=fruits.append("Tomato")
+print(fruits)
+
+fruits.insert(2,"Guvava")
+print(fruits)
+
+fruits.remove("Tomato")
+print(fruits)
+
+fruits.pop(2)
+print(fruits)
+
+fruits.sort()
+print(fruits)
+
+fruits.sort(reverse=True)
+print(fruits)
