8e351c006c38897195d12ef5c71317a60ec00e16
dewangupadhyay4 <dewangupadhyay23@gmail.com>
2025-05-14 21:32:22 +0530

    This is day 9 of learning python

--- Day9.py
+++ Day9.py
@@ -5,14 +5,25 @@
 
 #-----------------------------------------------------------------------------------
 
-# def adding_function(num1, num2):
+# def adding_function(num1, num2): #num1 and num2 are parameters
 #     return num1+num2
 
-# print(adding_function(5,8))
+# print(adding_function(5,8)) #5,8 are arguments
 
 #-----------------------------------------------------------------------------------------------------
 
-def another_addition(**args):
-    return sum(args)
+# def another_addition(*args):
+#     total=0
+#     for i in args:
+#         total += i
+#     return total
 
-print(another_addition(44,66,77))
+# print(another_addition(45,5,3))
+
+#-----------------------------------------------------------------------------------------------------
+
+def show_info(**kwargs):
+    for key, value in kwargs.items():
+        print(f"{key}: {value}")
+
+show_info(name="dewang", age=22, city="Mumbai")
