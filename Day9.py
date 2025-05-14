8beb582daae6354fa33503e6d55df43dd1ce4c79
dewangupadhyay4 <dewangupadhyay23@gmail.com>
2025-05-14 07:06:19 +0530

    This is day9 of learning python

---
+++ Day9.py
@@ -0,0 +1,18 @@
+# def add():
+#     print("Hello this function is just for calling purpose")
+
+# add()  #calling add funtion
+
+#-----------------------------------------------------------------------------------
+
+# def adding_function(num1, num2):
+#     return num1+num2
+
+# print(adding_function(5,8))
+
+#-----------------------------------------------------------------------------------------------------
+
+def another_addition(**args):
+    return sum(args)
+
+print(another_addition(44,66,77))
