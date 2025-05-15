c77924cc55dca2e71976115577e4c29f93da34b6
dewangupadhyay4 <dewangupadhyay23@gmail.com>
2025-05-15 06:49:16 +0530

    This is day 10 lambda series

--- Day9.py
+++ Day9.py
@@ -1,24 +1,24 @@
-# def add():
-#     print("Hello this function is just for calling purpose")
+def add():
+    print("Hello this function is just for calling purpose")
 
-# add()  #calling add funtion
+add()  #calling add funtion
 
 #-----------------------------------------------------------------------------------
 
-# def adding_function(num1, num2): #num1 and num2 are parameters
-#     return num1+num2
+def adding_function(num1, num2): #num1 and num2 are parameters
+    return num1+num2
 
-# print(adding_function(5,8)) #5,8 are arguments
+print(adding_function(5,8)) #5,8 are arguments
 
 #-----------------------------------------------------------------------------------------------------
 
-# def another_addition(*args):
-#     total=0
-#     for i in args:
-#         total += i
-#     return total
+def another_addition(*args):
+    total=0
+    for i in args:
+        total += i
+    return total
 
-# print(another_addition(45,5,3))
+print(another_addition(45,5,3))
 
 #-----------------------------------------------------------------------------------------------------
 
