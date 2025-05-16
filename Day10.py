7ee12774bde4e05cc006f842e006a8a23f4456dd
dewangupadhyay4 <dewangupadhyay23@gmail.com>
2025-05-16 07:02:07 +0530

    This is day 11 and learning list, dict comprehension

--- Day10.py
+++ Day10.py
@@ -1,25 +1,25 @@
 from functools import reduce
 
-# def square(num):
-#     return num*num
-# print(square(5))
+def square(num):
+    return num*num
+print(square(5))
 
 #----------------------------------------------------------------------------
 
-# square=lambda x : x*x
-# print(square(7))
+square=lambda x : x*x
+print(square(7))
 
 #----------------------------------------------------------------------------
 
-# nums=[2,4,6,8,9]
-# square=list(map(lambda x : x**2, nums))  #💡 Use when you want to apply a function to every item in a list.
-# print(square)
+nums=[2,4,6,8,9]
+square=list(map(lambda x : x**2, nums))  #💡 Use when you want to apply a function to every item in a list.
+print(square)
 
 #----------------------------------------------------------------------------
 
-# nums=[2,4,6,8,9,34,67,434,232,23233,23]
-# filtered=list(filter(lambda x : x%2==0, nums))    #💡 Use when you want to filter out some items based on a condition.
-# print(filtered)
+nums=[2,4,6,8,9,34,67,434,232,23233,23]
+filtered=list(filter(lambda x : x%2==0, nums))    #💡 Use when you want to filter out some items based on a condition.
+print(filtered)
 
 #----------------------------------------------------------------------------
 
---
+++ Day11.py
@@ -0,0 +1,21 @@
+# nums=[x for x in range(1,21) if x%3==0]
+# print(nums)
+
+#------------------------------------------------------------------------------------------------
+
+# listing=["Python", "is", "awesome"]
+# length=[len(listing) for x in listing]
+# print(length)
+
+#----------------------------------------------------------------------------------------------------
+
+people = {
+    "Dewang": 25,
+    "Riya": 17,
+    "Amit": 30,
+    "Rohan": 15,
+    "Sneha": 22
+}
+
+adults={name: age for name,age in people.items() if age >=18}
+print(adults)
