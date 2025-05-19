052364c6e48a20747861ece1bebe8af83a2bb427
dewangupadhyay4 <dewangupadhyay23@gmail.com>
2025-05-19 07:05:16 +0530

    This is day 15 of learning python

--- Day15.py
+++ Day15.py
@@ -1,14 +1,35 @@
 #inheritance
-class Employee:
-    def works(self):
-        print("This is Employee class")
+# class Employee:
+#     def works(self):
+#         print("This is Employee class")
 
-class Manager(Employee):
-    def works(self):
-        print("This is Manager class")
+# class Manager(Employee):
+#     def works(self):
+#         print("This is Manager class")
 
-employee=Employee()
-employee.works()
+# employee=Employee()
+# employee.works()
 
-manager=Manager()
-manager.works()
+# manager=Manager()
+# manager.works()
+
+#---------------------------------------------------------
+
+#Encapsulation
+
+class Car:
+    def __init__(self, brand, model, year):
+        self.brand=brand
+        self.__model=model
+        self.__year=year
+
+    def get_car(self):
+        return self.__model
+    
+    def get_details(self):
+        print(f"The car brand is {self.brand} , model is {self.__model} and year is {self.__year}")
+
+car= Car("Maruti","ABCS",2021)
+print(car.get_car())
+
+car.get_details()
