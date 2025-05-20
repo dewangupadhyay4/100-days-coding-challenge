ee8a79c78de580c04013d285889e80a7cbda2935
dewangupadhyay4 <dewangupadhyay23@gmail.com>
2025-05-20 07:00:42 +0530

    This is day 16 of learning python

---
+++ Day16.py
@@ -0,0 +1,19 @@
+def add(num1, num2):
+    return num1+num2
+
+def sub(num1, num2):
+    return num1-num2
+
+def mul(num1, num2):
+    return num1*num2
+
+def div(num1, num2):
+    return num1/num2
+
+# n1=int(input("Enter the first number: "))
+# n2=int(input("Enter the second number: "))
+
+# print(add(n1,n2))
+# print(sub(n1,n2))
+# print(mul(n1,n2))
+# print(div(n1,n2))
---
+++ Day16_part2.py
@@ -0,0 +1,15 @@
+import Day16 as day
+import random
+
+# print(day.add(22,21))
+
+# print(day.sub(22,1))
+
+# print(day.mul(44,1))
+
+# print(day.div(45,5))
+
+#----------------------------------------------------------
+
+print(random.randint(1,100))
+print(random.random())
