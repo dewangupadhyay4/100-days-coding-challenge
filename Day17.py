ef645ac75f3b55e31cd7b7e93ec420b4051a3a9f
dewangupadhyay4 <dewangupadhyay23@gmail.com>
2025-05-21 07:02:08 +0530

    This is day 17

---
+++ Day17.py
@@ -0,0 +1,11 @@
+# file=open("hello.txt","r",encoding='UTF-8')
+# print(file.read())
+# file.close()
+
+#-------------------------------------------------------------------------
+
+# with open("hello.txt","r") as file:
+#     for line in file:
+#         print(line.strip())
+
+#-------------------------------------------------------------------------
