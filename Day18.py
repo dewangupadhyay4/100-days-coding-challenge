86e0a950e1a6c3524d2a2beac816eefa063dafe4
dewangupadhyay4 <dewangupadhyay23@gmail.com>
2025-05-22 10:46:10 +0530

    This is day 18

---
+++ Day18.py
@@ -0,0 +1,12 @@
+import datetime
+from datetime import timedelta
+
+# now = datetime.datetime.now()
+# print("current date and time is ", now)
+# print("current date  is ", now.date())
+# print("current time is ", now.time())
+
+# print("")
+
+# birthdate=datetime.date(2003,10,22)
+# print(birthdate)
