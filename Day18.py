42d30d4bf087b53d6ecd7ea3b8057f8c16a7fbb3
dewangupadhyay4 <dewangupadhyay23@gmail.com>
2025-05-22 10:59:20 +0530

    This is day 18

--- Day18.py
+++ Day18.py
@@ -11,11 +11,17 @@ from datetime import timedelta
 # birthdate=datetime.date(2003,10,22)
 # print(birthdate)
 
+# today=datetime.datetime.today()
+# print("Today: ",today)
+
+# future=today+timedelta(days=12)
+# print("12 days later: ",future)
+
+# past=today-timedelta(days=12)
+# print("Before 12 days: ",past)
+
+birtday=input("Enter your Birthday: ")
+converted=datetime.datetime.strptime(birtday,"%d-%m-%y")
 today=datetime.datetime.today()
-print("Today: ",today)
-
-future=today+timedelta(days=12)
-print("12 days later: ",future)
-
-past=today-timedelta(days=12)
-print("Before 12 days: ",past)
+left=today-converted
+print(left)
