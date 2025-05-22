c6a9112dd2dbb159c9aae99ea31ad6e5e83db2e6
dewangupadhyay4 <dewangupadhyay23@gmail.com>
2025-05-22 11:12:25 +0530

    This is day 18

--- Day18.py
+++ Day18.py
@@ -20,8 +20,14 @@ from datetime import timedelta
 # past=today-timedelta(days=12)
 # print("Before 12 days: ",past)
 
-birtday=input("Enter your Birthday: ")
-converted=datetime.datetime.strptime(birtday,"%d-%m-%y")
+birthdate=input("Enter your birthdate: ")
+birth_date=datetime.datetime.strptime(birthdate,"%d-%m-%Y")
+
 today=datetime.datetime.today()
-left=today-converted
-print(left)
+this_year_birthday=birth_date.replace(year=today.year)
+
+if this_year_birthday < today:
+    this_year_birthday=this_year_birthday.replace(year=today.year+1)
+
+days_left=this_year_birthday - today
+print(days_left)
