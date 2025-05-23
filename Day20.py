89fdc584c22490c3eccf2d7b22746049ad6d7d28
dewangupadhyay4 <dewangupadhyay23@gmail.com>
2025-05-23 21:35:18 +0530

    This is day 20 learning python

--- Day20.py
+++ Day20.py
@@ -1,9 +1,29 @@
-class Person:
-    def __init__(self, name):
-        self.name=name
+# class Person:
+#     def __init__(self, name):
+#         self.name=name
 
-    def greet(self):
-        print(f"Hello this is {self.name}")
+#     def greet(self):
+#         print(f"Hello this is {self.name}")
 
-person=Person("Dewang")
-person.greet()
+# person=Person("Dewang")
+# person.greet()
+
+#---------------------------------------------------------------------------
+
+# class Employee:
+#     company="Tata Capital"
+
+#     @classmethod
+#     def show_company(cls):
+#         print(f"We work at {cls.company}")
+
+# Employee.show_company()
+
+#---------------------------------------------------------------------------
+
+class Calculator:
+    @staticmethod
+    def add(x, y):
+        return x + y
+    
+print(Calculator.add(5,6))
