89ab9ebb6b64b7b3d59765232fd8666e7a7e7e8c
dewangupadhyay4 <dewangupadhyay23@gmail.com>
2025-05-24 06:58:02 +0530

    This is day 21

--- Day21.py
+++ Day21.py
@@ -1,22 +1,42 @@
-class BankAccount:
-    def __init__(self, owner, balance):
-        self.owner=owner
-        self._bank="Tata Capital"    #protected
-        self.__balance=balance    #private
+# class BankAccount:
+#     def __init__(self, owner, balance):
+#         self.owner=owner
+#         self._bank="Tata Capital"    #protected
+#         self.__balance=balance    #private
 
-    def get_balance(self):
-        return self.__balance
+#     def get_balance(self):
+#         return self.__balance
     
-    def set_balance(self, amount):
-        if amount > 0:
-            self.__balance=amount
-        else:
-            print("Invalid Amount")
+#     def set_balance(self, amount):
+#         if amount > 0:
+#             self.__balance=amount
+#         else:
+#             print("Invalid Amount")
 
-acc=BankAccount("Dewang", 120000)
-print(acc.owner)
-print(acc._bank)
-print(acc.get_balance())
-acc.set_balance(30000)
-print(acc.get_balance())
-print(acc.__balance)  #will raise error since it is private
+# acc=BankAccount("Dewang", 120000)
+# print(acc.owner)
+# print(acc._bank)
+# print(acc.get_balance())
+# acc.set_balance(30000)
+# print(acc.get_balance())
+# print(acc.__balance)  #will raise error since it is private
+
+#--------------------------------------------------------------------------------------
+
+class Student:
+    def __init__(self,marks):
+        self.__marks=marks
+
+    def get_marks(self):
+        return self.__marks
+    
+    def set_marks(self,marks):
+        if marks >=0 & marks<=100:
+            self.__marks=marks
+        else:
+            print("Invalid marks")
+
+student=Student(34)
+print(student.get_marks())
+student.set_marks(-78)
+print(student.get_marks())
