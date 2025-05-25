8f392ae023133de7d3459c94db10a5855cc84064
dewangupadhyay4 <dewangupadhyay23@gmail.com>
2025-05-25 08:31:20 +0530

    new changes

--- Day26.py
+++ Day26.py
@@ -1,17 +1,27 @@
-class MyIterator:
+# class MyIterator:
 
-    def __iter__(self):
-        self.num=1
-        return self
+#     def __iter__(self):
+#         self.num=1
+#         return self
     
-    def __next__(self):
-        if self.num<=5:
-            val=self.num
-            self.num+=1
-            return val
-        else:
-            raise StopIteration
+#     def __next__(self):
+#         if self.num<=5:
+#             val=self.num
+#             self.num+=1
+#             return val
+#         else:
+#             raise StopIteration
         
-number=MyIterator()
-for i in number:
-    print(i)
+# number=MyIterator()
+# for i in number:
+#     print(i)
+
+#-----------------------------------------------------------------------------
+
+def even_number():
+    for i in range(1,11):
+        if i % 2 == 0:
+            yield i
+
+for num in even_number():
+    print(num)
