50b22a6deb9c939c55d119b9ae603b887a9b1f01
dewangupadhyay4 <dewangupadhyay23@gmail.com>
2025-05-25 10:36:52 +0530

    new changes

--- Day26.py
+++ Day26.py
@@ -1,20 +1,20 @@
-# class MyIterator:
+class MyIterator:
 
-#     def __iter__(self):
-#         self.num=1
-#         return self
+    def __iter__(self):
+        self.num=1
+        return self
     
-#     def __next__(self):
-#         if self.num<=5:
-#             val=self.num
-#             self.num+=1
-#             return val
-#         else:
-#             raise StopIteration
+    def __next__(self):
+        if self.num<=5:
+            val=self.num
+            self.num+=1
+            return val
+        else:
+            raise StopIteration
         
-# number=MyIterator()
-# for i in number:
-#     print(i)
+number=MyIterator()
+for i in number:
+    print(i)
 
 #-----------------------------------------------------------------------------
 
---
+++ Day27.py
@@ -0,0 +1,23 @@
+# def decorator_func(func):
+#     def wrapper():
+#         print("Before func runs")
+#         func()
+#         print("After func runs")
+#     return wrapper
+
+# def say_hello():
+#     print("Hello this is say_hello function")
+
+# # decorated=decorator_func(say_hello)
+# # decorated()
+
+# @decorator_func
+# def say_bye():
+#     print("Hello this bye function")
+
+# say_bye()
+
+#-------------------------------------------------------------------------------------------------------
+
+
+
