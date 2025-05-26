1f6723a14fe2a7a5e012252408effc3018d08c67
dewangupadhyay4 <dewangupadhyay23@gmail.com>
2025-05-26 06:55:26 +0530

    this is new change

---
+++ Day28.py
@@ -0,0 +1,19 @@
+#pip install django
+
+#django-admin startproject myproject
+#cd myproject
+
+# manage.py – Command-line tool to interact with the project.
+
+# settings.py – All config settings like DB, installed apps, etc.
+
+# urls.py – Root URL configuration.
+
+# wsgi.py/asgi.py – Entry points for deployment.
+
+#python manage.py runserver
+
+#python manage.py startapp myapp
+
+# 📌 5. Register the App
+# In myproject/settings.py, add your app to INSTALLED_APPS:
