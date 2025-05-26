5c6c0d26d1a6d6835e56295b498f54f8e8bcb79d
dewangupadhyay4 <dewangupadhyay23@gmail.com>
2025-05-26 07:16:44 +0530

    this is new change

--- django_projects/myproject/myapp/views.py
+++ django_projects/myproject/myapp/views.py
@@ -2,4 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("<h1>Hello this is home page")
+    # return HttpResponse("<h1>Hello this is home page")
+    return render(request, "myapp/home.html")
+
+def about(request):
+    # return HttpResponse("<h1>Hello this is home page")
+    return render(request, "myapp/about.html")
+
+def contact(request):
+    # return HttpResponse("<h1>Hello this is home page")
+    return render(request, "contact/home.html")
