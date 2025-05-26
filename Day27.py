91b4c3aa951ec7d4c3ad76050254bd7f1b6c30d2
dewangupadhyay4 <dewangupadhyay23@gmail.com>
2025-05-26 06:48:37 +0530

    this is new change

--- Day27.py
+++ Day27.py
@@ -1,3 +1,7 @@
+import time
+from functools import wraps
+import random
+
 # def decorator_func(func):
 #     def wrapper():
 #         print("Before func runs")
@@ -19,16 +23,37 @@
 
 #-------------------------------------------------------------------------------------------------------
 
-def decorated_func(func):
-    def wrapper(*args, **kwargs):
-        print("Before func runs")
+# def decorated_func(func):
+#     def wrapper(*args, **kwargs):
+#         print("Before func runs")
+#         result=func(*args, **kwargs)
+#         print("After func runs")
+#         return result
+#     return wrapper
+
+# @decorated_func
+# def greet(name):
+#     print(f"Hello {name}")
+
+# greet("dewang")
+
+#-------------------------------------------------------------------------------------------------------
+
+def timer(func):
+    @wraps(func)
+    def wrapper(*args,**kwargs):
+        start=time.time()
         result=func(*args, **kwargs)
-        print("After func runs")
+        end=time.time()
         return result
     return wrapper
 
-@decorated_func
-def greet(name):
-    print(f"Hello {name}")
+@timer
+def delayed_function():
+    sleeptime=random.randint(1,3)
+    print(f"Sleeptime in seconds {sleeptime}")
+    time.sleep(sleeptime)
+    print("function finished")
 
-greet("dewang")
+delayed_function()
+
