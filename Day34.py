2c649a63da3c8fbc2bb24f0e8187e26c07465449
dewangupadhyay4 <dewangupadhyay23@gmail.com>
2025-05-30 06:57:50 +0530

    This is day 34

---
+++ Day34.py
@@ -0,0 +1,3 @@
+🛠 Step-by-Step Implementation
+✅ 1. Create the Model
+In models.py of your app:
--- django_projects/myproject/myapp/forms.py
+++ django_projects/myproject/myapp/forms.py
@@ -1,7 +1,12 @@
 from django import forms
+from .models import Feedback
 
 class ContactForm(forms.Form):
     name=forms.CharField(max_length=100)
     email=forms.EmailField()
     message=forms.CharField(widget=forms.Textarea)
-    
+
+class Feedback(forms.ModelForm):
+    class Meta:
+        model=Feedback
+        field=['name','email','message']
