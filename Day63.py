7f64e38b26ddb8fa5046d7eaf4f5a3ef03d29163
dewangupadhyay4 <dewangupadhyay23@gmail.com>
2025-07-10 21:39:33 +0530

    This is new commit

--- Day63.py
+++ Day63.py
@@ -1,8 +1,28 @@
-list=[12,34,543,3,3,323,232,12,121,344]
+listing=[12,34,543,3,3,323,232,12,121,344]
 
-def find_lar_small(list):
-    largest=max(list)
-    smallest=min(list)
-    return largest, smallest
+# def find_lar_small(listing):
+#     largest=max(listing)
+#     smallest=min(listing)
+#     return largest, smallest
 
-print(find_lar_small(list))
+# print(find_lar_small(listing))
+
+# def reverse(listing):
+#     reversed=[]
+#     for i in range(len(listing)-1,-1,-1):
+#         reversed.append(listing[i])
+#     return reversed
+
+print(list(reversed(listing)))
+
+def count(listing):
+    count={}
+    for i in listing:
+        if i in count:
+            count[i]+=1
+        else:
+            count[i]=1
+
+    return count
+
+print(count(listing))
