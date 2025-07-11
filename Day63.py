5ba61fc31d954b6d3fb5bf89e2beb8d16335ee7d
dewangupadhyay4 <dewangupadhyay23@gmail.com>
2025-07-11 07:03:53 +0530

    This is new commit

--- Day63.py
+++ Day63.py
@@ -1,17 +1,19 @@
+from math import sqrt
+
 listing=[12,34,543,3,3,323,232,12,121,344]
 
-# def find_lar_small(listing):
-#     largest=max(listing)
-#     smallest=min(listing)
-#     return largest, smallest
+def find_lar_small(listing):
+    largest=max(listing)
+    smallest=min(listing)
+    return largest, smallest
 
-# print(find_lar_small(listing))
+print(find_lar_small(listing))
 
-# def reverse(listing):
-#     reversed=[]
-#     for i in range(len(listing)-1,-1,-1):
-#         reversed.append(listing[i])
-#     return reversed
+def reverse(listing):
+    reversed=[]
+    for i in range(len(listing)-1,-1,-1):
+        reversed.append(listing[i])
+    return reversed
 
 print(list(reversed(listing)))
 
@@ -26,3 +28,24 @@ def count(listing):
     return count
 
 print(count(listing))
+
+def isPrime(n):
+    if n < 1:
+        return False
+    if n==2:
+        return True
+    for i in range(2, int(sqrt(n))+1):
+        if n%i==0:
+            return False
+    return True
+    
+def sum(listing):
+    sum=0
+    for i in range(len(listing)):
+        if isPrime(listing[i]):
+            sum=sum+listing[i]
+    return sum
+        
+summ=sum(listing)
+print(summ)
+        
